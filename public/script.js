b797e9160d79ba2000858a0d2e394153ee37481e
Sherly Manggala <sherly_manggala@yahoo.com>
2017-11-08 11:25:34 +0700

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,109 +1,102 @@
-/*  
-###
-### Handles the sign in button press.
-###
-*/
-
-function toggleSignIn() {
-    if (firebase.auth.currentUser) {
-        // Start Signout
-        firebase.auth().signOut();
-    }
-    else {
-        var email = document.getElementById('txtEmail').value;
-        var password = document.getElementById('txtPassword').value;
-        if (email.length < 4) {
-            alert('Please enter an email address.');
-            return;
-        }
-        if (password.length < 4) {
-            alert('Please enter a password.');
-            return;
-        }
-        
-        // Sign in with email and password
-        // Start auth with email
-        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-            // Handle errors here
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            
-            //start exclude
-            if (errorCode === 'auth/wrong-password') {
-                alert('Wrong password.');
-            }
-            else {
-                alert(errorMessage);
-            }
-            console.log(error);
-            document.getElementById('quickstart-sign-in').disabled = false;
-            //end exclude
-        });
-        //end auth with email
-    }
-    document.getElementById('quickstart-sign-in').disabled = true;
-}
-
-/*
-###
-### Handles the sign up button press.
-###
-*/
-function handleSignUp() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('pass1').value;
-    var passTemp = document.getElementById('pass2').value;
-    if (passTemp != password) {
-        alert('Your password doesn\'t match.');
-        return;
-    }
-    if(email.length < 4) {
-        alert('Please enter a valid email address.');
-        return;
-    }
-    if (password.length < 4) {
-        alert('Please enter a password.');
-        return;
-    }
-    
-    //Sign in with email and pass.
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        //Handle error here
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        
-        //start exclude
-        if (errorCode == 'auth/weak-password') {
-            alert('The password is too weak.');
-        }
-        else {
-            alert(errorMessage);
-        }
-        console.log(error);
-        //end exclude
-    });
-}
-
-/*
-
-initApp handles setting up UI event listener and registering Firebase auth listeners
-
-*/
-function initApp() {
-    //Listening for auth state changes.
-    //start auth state listener
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-           // User is signed in
-           var email = user.email;
-           var uid = user.uid;
-           var providerData = user.providerData;
-           
-           document.getElementById('btnLogin').textContent = 'Log out';
-        }
-    });
-}
-
-window.onload = function() {
-    initApp();
-};
+//var firebase = require("firebase");
+//// Initialize Firebase
+//var config = {
+//    apiKey: "AIzaSyA_NK_kBO7DABY-7InCTNK9pNNITHnrlCE",
+//    authDomain: "multi-inventory.firebaseapp.com",
+//    databaseURL: "https://multi-inventory.firebaseio.com",
+//    projectId: "multi-inventory",
+//    storageBucket: "multi-inventory.appspot.com",
+//};
+//firebase.initializeApp(config);
+//var database = firebase.database();
+//
+//function handleLogout() {
+//    firebase.auth().signOut().then(function() {
+//        window.location = 'index.html';
+//    }).catch(function(error) {
+//        console.log(error);
+//    });
+//}
+//
+///* Read data from database */
+//function readUser(userId) {
+//    var userRef = firebase.database().ref('users/' + userId);
+//    var nameRef = userRef.child('name');
+//    nameRef.once("value").then(function(snapshot) {
+//        console.log(snapshot.val());
+//        document.getElementById('username').innerHTML = snapshot.val();
+//    });
+//}
+//
+// /* --- LOGIN FUNCTION --- */
+//function toggleSignIn() {
+//    if(firebase.auth().currentUser) {
+//        // [START signout]
+//        firebase.auth().signOut();
+//        // [END signout]
+//    } else {
+//        var email = document.getElementById('txtEmail').value;
+//        var password = document.getElementById('txtPassword').value;
+//        if (email.length < 4) {
+//            alert('Please enter a valid email address.');
+//            return;
+//        }
+//        if (password.length < 4) {
+//            alert('Please enter a password.');
+//            return;
+//        }
+//
+//        // Sign in with email and password.
+//        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+//           // Handle errors here.
+//            var errorCode = error.code;
+//            var errorMessage = error.message;
+//
+//            if (errorCode === 'auth/wrong-password') {
+//                alert('Wrong password.');
+//            } else {
+//                alert(errorMessage);
+//            }
+//            console.log(error);
+//        });
+//
+//        firebase.auth().onAuthStateChanged(function(user) {
+//        /* if user has already logged in or signed in, automaticaly redirect to home */
+//        if (user) {
+//            window.location = 'home.html';
+//        } 
+//    });
+//    }
+//}
+//
+//function initApp() {
+//    firebase.auth().onAuthStateChanged(function(user) {
+//        if (user) {
+//            readUser(user.uid);
+//            var userRef = database.ref('users/' + user.uid).child('role');
+//            userRef.once('value', function(snapshot) {
+//                console.log(snapshot.val());
+//
+//                if(snapshot.val() == 'user') {                                
+//                    //document.getElementsByClassName('userRole').remove();
+//                    var elements = document.getElementsByClassName('userRole');
+//                    while(elements.length > 0) {
+//                        elements[0].parentNode.removeChild(elements[0]);
+//                    }
+//                }
+//            });
+//            return;
+//        }
+//        /*else {
+//            window.location = 'index.html';
+//            alert('Error, you must log in first.');
+//        }*/
+//    });
+//
+//    document.getElementById('btnLogout').addEventListener('click', handleLogout, false);
+//    document.getElementById('btnLogin').addEventListener('click', toggleSignIn, false);
+//}
+//
+//window.onload = function() {
+//    initApp();
+//};
